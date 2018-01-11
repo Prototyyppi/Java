b1492e02ecc4acd40ba399cbbb873848264b6708
Unknown <jani.laitinen@gmx.com>
2018-01-11 13:18:22 +0200

    H2 START

---
+++ Product.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Integer;
+
+public class Product {
+    private String name;
+    private double price;
+    /* Setters */
+    public static String setName(String nameToSet) {
+        this.name = nameToSet;
+    }
+    public static String setName(double priceToSet) {
+        this.price = priceToSet;
+    }
+    /* Getters */
+    public static String getName {
+        return name;
+    }
+    public static double getPrice {
+        return price;
+    }
+    
